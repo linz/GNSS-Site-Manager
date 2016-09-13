77f41ca080faf3eded354bafb4a4453905f811f0
Brooke Smith <brooke.smith@ga.gov.au>
2016-09-14 09:05:15 +1000

    Create Jsonix service marshaller to XML

--- src/client/app/shared/jsonix/jsonix.service.ts
+++ src/client/app/shared/jsonix/jsonix.service.ts
@@ -18,10 +18,22 @@ let jsonIxMappings: any = [XLink_1_0, GML_3_2_1, GEODESYML_0_3, ISO19139_GMD_200
   ISO19139_GSR_20070417, ISO19139_GTS_20070417, ISO19139_GSS_20070417];
 
 let jsonIxOptions: any = {
-  mappingStyle: 'simplified'
+  mappingStyle: 'simplified',
+  namespacePrefixes: {
+    'http://www.opengis.net/cat/csw/2.0.2': 'csw',
+    'urn:xml-gov-au:icsm:egeodesy:0.3': 'geo',
+    'http://www.opengis.net/gml/3.2': 'gml',
+    'http://www.w3.org/1999/xlink': 'ns9',
+    'http://www.isotc211.org/2005/gmd': 'gmd',
+    'http://www.isotc211.org/2005/gmx': 'gmx',
+    'http://www.opengis.net/om/2.0': 'om',
+    'http://www.isotc211.org/2005/gco': 'gco',
+    'http://www.w3.org/2001/XMLSchema-instance': 'xsi'
+  }
 };
 let context: any = new Jsonix.Context(jsonIxMappings, jsonIxOptions);
 let unmarshaller: any = context.createUnmarshaller();
+let marshaller: any = context.createMarshaller();
 
 /**
  * This class provides the service to transfer data in GeodesyML format to and from the back end services, and translate to
@@ -34,16 +46,32 @@ export class JsonixService {
   static WS_URL: string = 'https://dev.geodesy.ga.gov.au'; // test
 
   /**
-   * Given valid GeodesyML instatnce, use translate to JSON.
+   * Given valid GeodesyML instatnce, translate to JSON.
    *
    * @param geodesyMl valid input to translate
    * @return The  JSON representation of the given geodesyMl
    */
   geodesyMlToJson(geodesyMl: string): string {
-    console.debug('geodesyMlToJson - geodesyMl (length): ', geodesyMl.length);
+    console.debug('JsonixService - geodesyMlToJson - geodesyMl (length): ', geodesyMl.length);
+    // console.debug('JsonixService - geodesyMlToJson - geodesyMl: ', geodesyMl);
 
     let json: string = unmarshaller.unmarshalString(geodesyMl);
-    console.log('JsonixService - translated JSON: ', json);
+    console.log('JsonixService - geodesyMlToJson - translated JSON: ', json);
     return json;
   };
+
+  /**
+   * Given 'valid' JSON insance, translate to valid GeodesyMl.
+   * @param json that is 'valid' to translate
+   * @returns {string} the valid GeodesyMl
+   */
+  jsonToGeodesyMl(json: string): string {
+    let jsonString: string = JSON.stringify(json);
+    console.debug('JsonixService - jsonToGeodesyMl - json (length): ', jsonString.length);
+    // console.debug('JsonixService - jsonToGeodesyMl - json: ', jsonString);
+
+    let geodesyMl: string = marshaller.marshalString(json);
+    console.log('JsonixService - jsonToGeodesyMl - translated geodesyMl: ', geodesyMl);
+    return geodesyMl;
+  }
 }
