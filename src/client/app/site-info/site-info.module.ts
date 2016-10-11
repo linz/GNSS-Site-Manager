60eefed9b248abd551466472b4fd3bc6b9eba617
lbodor <lazar.bodor@ga.gov.au>
2016-10-11 14:28:06 +1100

    Use single quotes

--- src/client/app/site-info/site-info.module.ts
+++ src/client/app/site-info/site-info.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { SiteInfoComponent } from './site-info.component';
-import {GnssReceiverInfoModule} from "../gnss-receiver-info/gnss-receiver-info.module";
+import {GnssReceiverInfoModule} from '../gnss-receiver-info/gnss-receiver-info.module';
 
 @NgModule({
   imports: [CommonModule, SharedModule, GnssReceiverInfoModule],
