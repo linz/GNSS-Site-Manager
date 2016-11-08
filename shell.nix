863f605b870845c59741ea46d406d94349f57cac
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-11-09 09:10:31 +1100

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,20 @@
 let
-  pkgs = import <nixpkgs> {};
-  nodejs = with pkgs; buildEnv {
-    name = "nodejs";
+  defaultPkgs = import <nixpkgs> {};
+  pinnedPkgs = import (defaultPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "354fd3728952c229fee4f2924737c601d7ab4725";
+    sha256 = "052hfh3w2086zaaskx0y0dz4y6y2bpvj0xqlcarpbwn2ccnpph0j";
+  }) {};
+
+in
+
+{ nixpkgs ? pinnedPkgs }:
+
+let
+  pkgs = if nixpkgs == null then defaultPkgs else pinnedPkgs;
+  devEnv = with pkgs; buildEnv {
+    name = "devEnv";
     paths = [
       nodejs-6_x
       awscli
@@ -10,7 +23,7 @@ let
 in
   pkgs.runCommand "setupEnv" {
     buildInputs = [
-      nodejs
+      devEnv
     ];
     shellHook = ''
       if [[ -e ./node_modules ]]; then
@@ -18,4 +31,3 @@ in
       fi
     '';
   } ""
-
