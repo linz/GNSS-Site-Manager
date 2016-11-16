46200d1ff64a9950aa4d84055166d540d405af54
Ted Zhou <ted.zhou@ga.gov.au>
2016-11-16 12:37:59 +1100

    Added episodic effects

--- src/client/app/site-info/site-info.module.ts
+++ src/client/app/site-info/site-info.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from '../shared/shared.module';
 import { SiteInfoComponent } from './site-info.component';
 import { GnssReceiverInfoModule } from '../gnss-receiver-info/gnss-receiver-info.module';
 import { FrequencyStandardModule } from '../frequency-standard/frequency-standard.module';
+import { EpisodicEffectModule } from '../episodic-effect/episodic-effect.module';
 import { DatetimePickerModule } from '../datetime-picker/datetime-picker.module';
 import { HumiditySensorModule } from '../sensor-humidity/humidity-sensor.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, GnssReceiverInfoModule, DatetimePickerModule, HumiditySensorModule, FrequencyStandardModule],
+  imports: [CommonModule, SharedModule, GnssReceiverInfoModule, DatetimePickerModule, HumiditySensorModule, EpisodicEffectModule, FrequencyStandardModule],
   declarations: [SiteInfoComponent],
   exports: [SiteInfoComponent],
 })
+
 export class SiteInfoModule { }
