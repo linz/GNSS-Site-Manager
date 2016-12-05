5a0bd4aea6781d01da636fbdc0512cf0ce307b8e
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-12-06 10:43:54 +1100

    Run prod build on pull requests

--- travis/build.sh
+++ travis/build.sh
@@ -6,9 +6,9 @@ set -e
 npm install
 typings install
 xvfb-run npm run test
+npm run build.prod -- --config-env test
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    npm run build.prod -- --config-env test
     aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}" --profile geodesy
     aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}" --profile geodesy
     aws configure set region ap-southeast-2 --profile geodesy
