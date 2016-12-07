139fcbae8101484e1df545611a7ef625f0a9e737
Brooke Smith <brooke.smith@ga.gov.au>
2016-12-07 11:45:53 +1100

    Json pointer service statics updated test

--- src/client/app/shared/json-pointer/json-pointer.service.spec.ts
+++ src/client/app/shared/json-pointer/json-pointer.service.spec.ts
@@ -1,14 +1,9 @@
-import { ReflectiveInjector } from '@angular/core';
-import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-
-import { HttpUtilsService } from '../global/http-utils.service';
 import { JsonPointerService } from './json-pointer.service';
 
 
 export function main() {
   describe('Testing of JsonPointerService', () => {
-    let jsonPathService: JsonPointerService;
+    // let JsonPointerService: JsonPointerService;
     let jsonObj: any = {
       standardType: { value: '' },
       inputFrequency: '',
@@ -40,49 +35,31 @@ export function main() {
       }
     };
 
-    beforeEach(() => {
-
-      let injector = ReflectiveInjector.resolveAndCreate([
-        JsonPointerService,
-        HttpUtilsService,
-        BaseRequestOptions,
-        MockBackend,
-        {
-          provide: Http,
-          useFactory: function (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
-            return new Http(backend, defaultOptions);
-          },
-          deps: [MockBackend, BaseRequestOptions]
-        },
-      ]);
-      jsonPathService = injector.get(JsonPointerService);
-    });
-
     it('JsonPointerService should be defined', () => {
       expect(JsonPointerService).not.toBeUndefined();
     });
 
     it('Test json pointer path - exists(), get() ... ...', () => {
       expect(jsonObj).toBeDefined();
-      expect(jsonPathService.exists(jsonObj, '/standardType/value')).toEqual(true);
-      expect(jsonPathService.exists(jsonObj, '/individualName/characterString')).toEqual(true);
-      expect(jsonPathService.get(jsonObj, '/inputFrequency')).toEqual('');
-      expect(jsonPathService.get(jsonObj, '/notes')).toEqual('Testing');
+      expect(JsonPointerService.exists(jsonObj, '/standardType/value')).toEqual(true);
+      expect(JsonPointerService.exists(jsonObj, '/individualName/characterString')).toEqual(true);
+      expect(JsonPointerService.get(jsonObj, '/inputFrequency')).toEqual('');
+      expect(JsonPointerService.get(jsonObj, '/notes')).toEqual('Testing');
     });
 
     it('Test path with string-attribute ... ...', () => {
-      expect(jsonPathService.exists(jsonObj, '/organisationName/characterString/gco:CharacterString')).toEqual(true);
-      expect(jsonPathService.get(jsonObj, '/individualName/characterString/gco:CharacterString')).toEqual('Mark Smith');
+      expect(JsonPointerService.exists(jsonObj, '/organisationName/characterString/gco:CharacterString')).toEqual(true);
+      expect(JsonPointerService.get(jsonObj, '/individualName/characterString/gco:CharacterString')).toEqual('Mark Smith');
     });
 
     it('Test path with array ... ...', () => {
-      var item: any = jsonPathService.get(jsonObj,
+      var item: any = JsonPointerService.get(jsonObj,
                      '/this/path/doesnt/exist');
       expect(item).toBeNull();
-      expect(jsonPathService.exists(jsonObj,
+      expect(JsonPointerService.exists(jsonObj,
                     '/contactInfo/ciContact/address/ciAddress/deliveryPoint/0'))
                     .toEqual(true);
-      expect(jsonPathService.get(jsonObj,
+      expect(JsonPointerService.get(jsonObj,
               '/contactInfo/ciContact/address/ciAddress/deliveryPoint/0/characterString/gco:CharacterString'))
               .toEqual('8 Kemba Street, Mitchell, ACT');
     });
@@ -90,17 +67,17 @@ export function main() {
     it('Test setting with existing path', () => {
       let path: string = '/organisationName/characterString/gco:CharacterString';
       let newValue: string = 'victor hugo';
-      expect(jsonPathService.exists(jsonObj, path)).toEqual(true);
-      jsonPathService.set(jsonObj, path, newValue);
-      expect(jsonPathService.get(jsonObj, path)).toEqual(newValue);
+      expect(JsonPointerService.exists(jsonObj, path)).toEqual(true);
+      JsonPointerService.set(jsonObj, path, newValue);
+      expect(JsonPointerService.get(jsonObj, path)).toEqual(newValue);
     });
 
     it('Test setting with NON-existing path - it should then exist', () => {
       let path: string = '/this/path/doesnt/exist';
       let newValue: string = 'victor hugo';
-      expect(jsonPathService.exists(jsonObj, path)).toEqual(false);
-      jsonPathService.set(jsonObj, path, newValue);
-      expect(jsonPathService.get(jsonObj, path)).toEqual(newValue);
+      expect(JsonPointerService.exists(jsonObj, path)).toEqual(false);
+      JsonPointerService.set(jsonObj, path, newValue);
+      expect(JsonPointerService.get(jsonObj, path)).toEqual(newValue);
     });
   });
 }
