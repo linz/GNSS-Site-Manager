fbfee2cf1ce5fee984d234fd075377c2c36314f6
Heya <heya.gosper@ga.gov.au>
2017-01-04 16:37:27 +1100

    delete unused files from humidity sensors directory
    clean up the humisity sensors directory

--- src/client/app/humidity-sensor/humidity-sensor.module.ts
+++ src/client/app/humidity-sensor/humidity-sensor.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { GnssHumiditySensorComponent } from './humidity-sensor.component';
 import { HumiditySensorsGroupComponent } from './humidity-sensors-group.component';
 import { HumiditySensorItemComponent } from './humidity-sensor-item.component';
 import { DatetimePickerModule } from '../datetime-picker/datetime-picker.module';
 
 @NgModule({
   imports: [CommonModule, FormsModule, DatetimePickerModule],
-  declarations: [GnssHumiditySensorComponent, HumiditySensorsGroupComponent, HumiditySensorItemComponent],
-  exports: [GnssHumiditySensorComponent, HumiditySensorsGroupComponent, HumiditySensorItemComponent],
+  declarations: [HumiditySensorsGroupComponent, HumiditySensorItemComponent],
+  exports: [ HumiditySensorsGroupComponent, HumiditySensorItemComponent],
 })
 export class HumiditySensorModule { }
