4e14766d3f7b3fd775e955fc42dc2e7d63f7bb69
Frank Fu <frank.fu@ga.gov.au>
2017-03-02 11:52:23 +1100

    Remove DatetimePicker component and apply DatetimeInput component to Antenna and FrequencyStandard

--- src/client/app/gnss-antenna/gnss-antenna.module.ts
+++ src/client/app/gnss-antenna/gnss-antenna.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GnssAntennaItemComponent } from './gnss-antenna-item.component';
 import { GnssAntennaGroupComponent } from './gnss-antenna-group.component';
-import { DatetimePickerModule } from '../datetime-picker/datetime-picker.module';
+import { DynamicFormFieldsModule } from '../shared/dynamic-form-fields/dynamic-form-fields.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, DatetimePickerModule],
+  imports: [CommonModule, FormsModule, DynamicFormFieldsModule],
   declarations: [GnssAntennaItemComponent, GnssAntennaGroupComponent],
   exports: [GnssAntennaItemComponent, GnssAntennaGroupComponent],
 })
