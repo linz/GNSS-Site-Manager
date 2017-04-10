0a972445c956edc1625edca5a498749290817ca4
lbodor <lazar.bodor@ga.gov.au>
2017-04-10 15:23:52 +1000

    Switch to devgeodesy-openam

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -4,7 +4,7 @@ const DevConfig: EnvConfig = {
   ENV: 'DEV',
   WEB_SERVICE_URL: 'https://devgeodesy-webservices.geodesy.ga.gov.au',
   WFS_GEOSERVER_URL: 'https://devgeodesy-geoserver.geodesy.ga.gov.au/geoserver/wfs',
-  OPENAM_SERVER_URL: 'https://devopenam-openam.geodesy.ga.gov.au/openam',
+  OPENAM_SERVER_URL: 'https://devgeodesy-openam.geodesy.ga.gov.au/openam',
   CLIENT_URL: 'https://dev.gnss-site-manager.geodesy.ga.gov.au'
 };
 
