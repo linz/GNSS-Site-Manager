3c303aab672ca89c0e1fdf53558c8f48273a4c5d
Brooke Smith <brooke.smith@ga.gov.au>
2017-05-03 13:13:17 +1000

    GEOD-417 This version works mostly
    * The ng-(in)valid, ng-dirty, ng-pristine set correctly
    on textarea-input's textarea BUT NOT INITIALY
    * I want to fix so it does so initially and I'd prefer NOT
    do an [ngClass] to set the classes but rather get the
    values from the parent (host) textarea-input open tag

--- src/client/app/shared/dynamic-form-fields/abstract-gnss-controls.ts
+++ src/client/app/shared/dynamic-form-fields/abstract-gnss-controls.ts
@@ -12,12 +12,12 @@ export abstract class AbstractGnssControls {
         let control: AbstractControl;
         // Force an error as this field should have been set
         control = this.superForm.controls[controlId];
-        if (! control) {
-            console.warn('Control "'+controlId+'" doesnt exist on form.');
+        if (!control) {
+            console.warn('Control "' + controlId + '" doesnt exist on form.');
             return errString;
         }
-
         if (control.errors) {
+            console.log('error report - control: ', controlId);
             for (let e of Object.keys(control.errors)) {
                 console.log(e + ' -> ', control.errors[e]);
                 errString += e;
@@ -26,5 +26,39 @@ export abstract class AbstractGnssControls {
             }
         }
         return errString;
+    }
+
+    protected isDirty(controlId: string): boolean {
+        let control: AbstractControl;
+        let valid: boolean = true;
+        // Force an error as this field should have been set
+        control = this.superForm.controls[controlId];
+        if (!control || !control.errors) {
+            valid = true;
+        } else {
+            valid = control.dirty;
+        }
+        console.warn('  isValid Control "' + controlId + ': '+ valid + ' - ', control);
+        return valid;
+    }
+
+    protected isValid(controlId: string): boolean {
+        let control: AbstractControl;
+        let valid: boolean = true;
+        // Force an error as this field should have been set
+        control = this.superForm.controls[controlId];
+        if (!control || !control.errors) {
+            valid = true;
+        } else {
+            valid = control.valid;  //(Object.keys(control.errors).length === 0);
+        }
+        console.warn('  isValid Control "' + controlId + ': '+ valid + ' - ', control);
+        return valid;
+    }
+
+    protected isInValid(controlId: string): boolean {
+        let valid: boolean = this.isValid(controlId);
+        // console.debug(`isInValid for ${controlId} - ${! valid}`);
+        return !valid;
     }
 }
