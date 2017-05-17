209d8854f380839beb0b6b16441790f850987cd2
lbodor <lazar.bodor@ga.gov.au>
2017-05-17 18:41:39 +1000

    Indent

--- src/client/app/select-site/select-site.component.ts
+++ src/client/app/select-site/select-site.component.ts
@@ -9,175 +9,175 @@ import { DialogService, CorsSiteService, ServiceWorkerService } from '../shared/
  * This class represents the SelectSiteComponent for searching and selecting CORS sites.
  */
 @Component({
-  moduleId: module.id,
-  selector: 'sd-select-site',
-  templateUrl: 'select-site.component.html',
+    moduleId: module.id,
+    selector: 'sd-select-site',
+    templateUrl: 'select-site.component.html',
 })
 export class SelectSiteComponent implements OnInit {
-  public columns: Array<any> = [
-    {name: 'fourCharacterId', sort: ''},
-    {name: 'name', sort: ''}
-  ];
+    public columns: Array<any> = [
+        {name: 'fourCharacterId', sort: ''},
+        {name: 'name', sort: ''}
+    ];
 
-  private serviceWorkerSubscription: Subscription;
-  private searchTextSubject = new Subject<string>();
-  private searchText: string = '';
-  private sites: Array<any> = [];
-  private selectedSite: any = null;
-  private searchMsg: string = '';
-  private errorMessage: string;
-  private isSearching: boolean = false;
-  private cacheItems: Array<string> = [];
+    private serviceWorkerSubscription: Subscription;
+    private searchTextSubject = new Subject<string>();
+    private searchText: string = '';
+    private sites: Array<any> = [];
+    private selectedSite: any = null;
+    private searchMsg: string = '';
+    private errorMessage: string;
+    private isSearching: boolean = false;
+    private cacheItems: Array<string> = [];
 
-  /**
-   * Creates an instance of the SelectSiteComponent with the injected CorsSiteService.
-   *
-   * @param {Router} router - The injected Router for switching between select-site and site-log pages.
-   * @param {CorsSiteService} corsSiteService - The injected CorsSiteService.
-   * @param {DialogService} dialogService - The injected DialogService.
-   * @param {ServiceWorkerService} serviceWorkerService - service interface to the Service Worker
-   */
-  constructor(public router: Router,
-              public corsSiteService: CorsSiteService,
-              private dialogService: DialogService,
-              private serviceWorkerService: ServiceWorkerService) { }
+    /**
+     * Creates an instance of the SelectSiteComponent with the injected CorsSiteService.
+     *
+     * @param {Router} router - The injected Router for switching between select-site and site-log pages.
+     * @param {CorsSiteService} corsSiteService - The injected CorsSiteService.
+     * @param {DialogService} dialogService - The injected DialogService.
+     * @param {ServiceWorkerService} serviceWorkerService - service interface to the Service Worker
+     */
+    constructor(public router: Router,
+        public corsSiteService: CorsSiteService,
+        private dialogService: DialogService,
+        private serviceWorkerService: ServiceWorkerService) { }
 
-  /**
-   * Initialize relevant variables when the directive is instantiated
-   */
-  ngOnInit() {
-    this.searchTextSubject
-        .debounceTime(400)
-        .subscribe((newText: string) => this.onSearchTextChange(newText));
-    this.setupSubscriptions();
-    this.clearAll();
-    this.updateCacheList();
-  }
-
-  setupSubscriptions() {
-    this.serviceWorkerSubscription = this.serviceWorkerService.clearCacheObservable.subscribe((isCacheChanged: boolean) => {
-      if (isCacheChanged) {
+    /**
+     * Initialize relevant variables when the directive is instantiated
+     */
+    ngOnInit() {
+        this.searchTextSubject
+            .debounceTime(400)
+            .subscribe((newText: string) => this.onSearchTextChange(newText));
+        this.setupSubscriptions();
+        this.clearAll();
         this.updateCacheList();
-      }
-    });
-  }
-
-  /**
-   * Run search function if users type any characters in "SearchText" input field, triggered by "change" event.
-   *
-   * Note: use ngModelChange event as it includes "keyup" event, paste and selection from dropdown hints.
-   */
-  public onSearchTextChange(value: string) {
-    this.searchText = value;
-    if (this.searchText === null || this.searchText.trim() === '') {
-      this.clearAll();
-    } else {
-      this.searchSites();
     }
-  }
 
-  /**
-   * Return a list of sites from DB based on the site name and/or four character Id.  Using WFS and XML.
-   */
-  public searchSites() {
-    // New rule: search text could be either Site Id or Site Name
-    let fourCharacterId: string = this.searchText;
-    let siteName: string = this.searchText;
+    setupSubscriptions() {
+        this.serviceWorkerSubscription = this.serviceWorkerService.clearCacheObservable.subscribe((isCacheChanged: boolean) => {
+            if (isCacheChanged) {
+                this.updateCacheList();
+            }
+        });
+    }
 
-    this.errorMessage = null;
-    this.isSearching = true;
-    this.sites = [];
-    this.corsSiteService.getCorsSitesByUsingWFS(fourCharacterId, siteName)
-      .subscribe(
-        (responseJson: any) => this.sites = responseJson,
-        (error: Error) => this.errorMessage = <any>error,
-        () => {
-          this.isSearching = false;
-          if (this.sites.length === 0) {
-            this.searchMsg = 'No sites found. Please refine your search criteria and try it again.';
-          } else {
-            let msg: string = this.sites.length + (this.sites.length === 1 ? ' site' : ' sites') + ' found.';
-            this.dialogService.showSuccessMessage(msg);
-          }
+    /**
+     * Run search function if users type any characters in "SearchText" input field, triggered by "change" event.
+     *
+     * Note: use ngModelChange event as it includes "keyup" event, paste and selection from dropdown hints.
+     */
+    public onSearchTextChange(value: string) {
+        this.searchText = value;
+        if (this.searchText === null || this.searchText.trim() === '') {
+            this.clearAll();
+        } else {
+            this.searchSites();
         }
-      );
-  }
-
-  /**
-   * Select a site from the search results of sites.
-   */
-  public selectSite(site: any) {
-    this.selectedSite = site;
-    let link = ['/siteLog', site.fourCharacterId];
-    this.router.navigate(link);
-  }
-
-  /**
-   * Clear all input fields and search results of sites
-   */
-  public clearAll() {
-    this.errorMessage = null;
-    this.searchMsg = 'Please enter search criteria for searching desired sites.';
-    this.searchText = '';
-    this.sites.length = 0;
-    this.selectedSite = null;
-    this.isSearching = false;
-  }
-
-  public getSortingOrder(index: number): string {
-    if (index < 0 || index >= this.columns.length) {
-      return 'ascending';
-    } else if (!this.columns[index].sort || this.columns[index].sort === 'asc') {
-      return 'ascending';
-    } else {
-      return 'descending';
     }
-  }
 
-  public sortField(columnIndex: number):any {
-    let sort = this.columns[columnIndex].sort;
-    for (let i = 0; i < this.columns.length; i ++) {
-      if ( i === columnIndex) {
-        if (!sort) {
-          sort = 'asc';
+    /**
+     * Return a list of sites from DB based on the site name and/or four character Id.  Using WFS and XML.
+     */
+    public searchSites() {
+        // New rule: search text could be either Site Id or Site Name
+        let fourCharacterId: string = this.searchText;
+        let siteName: string = this.searchText;
+
+        this.errorMessage = null;
+        this.isSearching = true;
+        this.sites = [];
+        this.corsSiteService.getCorsSitesByUsingWFS(fourCharacterId, siteName)
+            .subscribe(
+                (responseJson: any) => this.sites = responseJson,
+                (error: Error) => this.errorMessage = <any>error,
+                () => {
+                    this.isSearching = false;
+                    if (this.sites.length === 0) {
+                        this.searchMsg = 'No sites found. Please refine your search criteria and try it again.';
+                    } else {
+                        let msg: string = this.sites.length + (this.sites.length === 1 ? ' site' : ' sites') + ' found.';
+                        this.dialogService.showSuccessMessage(msg);
+                    }
+                }
+            );
+    }
+
+    /**
+     * Select a site from the search results of sites.
+     */
+    public selectSite(site: any) {
+        this.selectedSite = site;
+        let link = ['/siteLog', site.fourCharacterId];
+        this.router.navigate(link);
+    }
+
+    /**
+     * Clear all input fields and search results of sites
+     */
+    public clearAll() {
+        this.errorMessage = null;
+        this.searchMsg = 'Please enter search criteria for searching desired sites.';
+        this.searchText = '';
+        this.sites.length = 0;
+        this.selectedSite = null;
+        this.isSearching = false;
+    }
+
+    public getSortingOrder(index: number): string {
+        if (index < 0 || index >= this.columns.length) {
+            return 'ascending';
+        } else if (!this.columns[index].sort || this.columns[index].sort === 'asc') {
+            return 'ascending';
+        } else {
+            return 'descending';
         }
-        this.columns[i].sort = (sort === 'asc') ? 'desc' : 'asc';
-      } else {
-        this.columns[i].sort = '';
-      }
     }
 
-    let columnName = this.columns[columnIndex].name;
-    this.sites = this.sites.sort((previous:any, current:any) => {
-      if (previous[columnName] > current[columnName]) {
-        return sort === 'desc' ? -1 : 1;
-      } else if (previous[columnName] < current[columnName]) {
-        return sort === 'asc' ? -1 : 1;
-      }
-      return 0;
-    });
-  }
+    public sortField(columnIndex: number):any {
+        let sort = this.columns[columnIndex].sort;
+        for (let i = 0; i < this.columns.length; i ++) {
+            if (i === columnIndex) {
+                if (!sort) {
+                    sort = 'asc';
+                }
+                this.columns[i].sort = (sort === 'asc') ? 'desc' : 'asc';
+            } else {
+                this.columns[i].sort = '';
+            }
+        }
 
-  /**
-   * Component method to request the Service Worker clears it's cache.
-   */
-  clearCache = (): void  => {
-    this.serviceWorkerService.clearCache().then((data: string) => {
-      console.debug('select-site.component clearCache() success: ', data);
-    }, (error: Error) => {
-      throw new Error('Error in clearCache: ' + error.message);
-    });
-  }
+        let columnName = this.columns[columnIndex].name;
+        this.sites = this.sites.sort((previous:any, current:any) => {
+            if (previous[columnName] > current[columnName]) {
+                return sort === 'desc' ? -1 : 1;
+            } else if (previous[columnName] < current[columnName]) {
+                return sort === 'asc' ? -1 : 1;
+            }
+            return 0;
+        });
+    }
 
-  /**
-   * Component method to retrieve the list of URLs cached in the Service Worker and to update the this.cacheItem array
-   */
-  updateCacheList = (): void => {
-    this.serviceWorkerService.getCacheList().then((data: string[]) => {
-      this.cacheItems.length = 0;
-      this.cacheItems = data;
-    }).catch((error: any) => {
-      console.error('Caught error in updateCacheList:', error);
-    });
-  }
+    /**
+     * Component method to request the Service Worker clears it's cache.
+     */
+    clearCache = (): void  => {
+        this.serviceWorkerService.clearCache().then((data: string) => {
+            console.debug('select-site.component clearCache() success: ', data);
+        }, (error: Error) => {
+            throw new Error('Error in clearCache: ' + error.message);
+        });
+    }
+
+    /**
+     * Component method to retrieve the list of URLs cached in the Service Worker and to update the this.cacheItem array
+     */
+    updateCacheList = (): void => {
+        this.serviceWorkerService.getCacheList().then((data: string[]) => {
+            this.cacheItems.length = 0;
+            this.cacheItems = data;
+        }).catch((error: any) => {
+            console.error('Caught error in updateCacheList:', error);
+        });
+    }
 }
