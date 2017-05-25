4e25cd07d0ac8212929f8089c60da0cd5576e018
Lazar Bodor <lazar.bodor@ga.gov.au>
2017-05-25 15:28:03 +1000

    Fix indentation

--- src/client/app/shared/abstract-groups-items/abstract-group.component.ts
+++ src/client/app/shared/abstract-groups-items/abstract-group.component.ts
@@ -29,7 +29,7 @@ export abstract class AbstractGroupComponent<T extends AbstractViewModel> extend
     }
 
     ngOnInit() {
-           this.setupForm(this.getControlName());
+        this.setupForm(this.getControlName());
     }
 
     /**
