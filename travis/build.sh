252b1e488e20b3055350f10843404342d8d9168b
lbodor <lazar.bodor@ga.gov.au>
2017-05-31 11:07:39 +1000

    Deploy branch `master` to test

--- travis/build.sh
+++ travis/build.sh
@@ -3,12 +3,22 @@
 set -e
 
 npm run tests.all
-npm run build.prod -- --config-env dev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "next" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}" --profile geodesy
     aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}" --profile geodesy
     aws configure set region ap-southeast-2 --profile geodesy
     aws configure set output json --profile geodesy
-    ./deploy.sh dev
+    case "${TRAVIS_BRANCH}" in
+        "next")
+            npm run build.prod -- --config-env dev
+            ./deploy.sh dev
+        ;;
+        "master")
+            npm run build.prod -- --config-env test
+            ./deploy.sh test
+        ;;
+    esac
+else
+    npm run build.prod -- --config-env dev
 fi
