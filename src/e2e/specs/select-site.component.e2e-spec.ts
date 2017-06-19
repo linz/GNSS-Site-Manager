65fbdc850914cca119f0534b0d1c89b8d8333905
Brooke Smith <brooke.smith@ga.gov.au>
2017-06-19 15:55:27 +1000

    GEOD-514 - Convert to using Page Objects in our e2e tests
    * SelectSite for this one

--- src/e2e/specs/select-site.component.e2e-spec.ts
+++ src/e2e/specs/select-site.component.e2e-spec.ts
@@ -1,17 +1,18 @@
 import { browser, element, by } from 'protractor';
+import { SelectSitePage } from './select-site.pageobject';
 
 describe('SelectSite', () => {
+    let selectSitePage: SelectSitePage = new SelectSitePage();
 
-  beforeEach(async () => {
-        return await browser.get('/');
-  });
+    beforeEach(async () => {
+        return await browser.get(selectSitePage.url);
+    });
 
-  it('should have an input for siteName/fourCharacterId', () => {
-    expect(element(by.css('sd-select-site input[name="searchText"]')).isPresent()).toEqual(true);
-  });
+    it('should have an input for siteName/fourCharacterId', () => {
+        expect(selectSitePage.searchBox.isPresent()).toEqual(true);
+    });
 
-  it('should have a search button', () => {
-    expect(element(by.css('sd-select-site button')).isPresent()).toEqual(true);
-  });
-
+    it('should have a search button', () => {
+        expect(selectSitePage.searchButton.isPresent()).toEqual(true);
+    });
 });
