9a84377de9a059162ef5a9f0b1d9ae94f90d8bfb
Brooke Smith <brooke.smith@ga.gov.au>
2017-06-20 12:01:00 +1000

    GEOD-514 - PageObject for Select Site with more tests

--- src/e2e/specs/select-site.component.e2e-spec.ts
+++ src/e2e/specs/select-site.component.e2e-spec.ts
@@ -8,11 +8,27 @@ describe('SelectSite', () => {
         return await browser.get(selectSitePage.url);
     });
 
-    it('should have an input for siteName/fourCharacterId', () => {
-        expect(selectSitePage.searchBox.isPresent()).toEqual(true);
+    it('entering search text should return list of sites - one site', () => {
+        selectSitePage.searchFor('ade1');
+        expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
+        expect(selectSitePage.selectSiteListItems.count()).toEqual(1);
+        expect(selectSitePage.selectSiteListItems.first().getText()).toBe('ADE1');
+        selectSitePage.debug(selectSitePage.selectSiteList);
     });
 
-    it('should have a search button', () => {
-        expect(selectSitePage.searchButton.isPresent()).toEqual(true);
+    it('entering search text but do not click on button should return list of sites - one site', () => {
+        selectSitePage.enterSearchText('ade1');
+        expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
+        expect(selectSitePage.selectSiteListItems.count()).toEqual(1);
+        expect(selectSitePage.selectSiteListItems.first().getText()).toBe('ADE1');
+        selectSitePage.debug(selectSitePage.selectSiteList);
+    });
+
+    it('entering search text should return the specific sites - multiple sites', () => {
+        selectSitePage.searchFor('ade');
+        expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
+        expect(selectSitePage.selectSiteListItems.count()).toBeGreaterThan(1);
+        expect(selectSitePage.elementArrayContaining(selectSitePage.selectSiteListItems, 'ADE1').count()).toBe(1);
+        selectSitePage.debugArray(selectSitePage.elementArrayContaining(selectSitePage.selectSiteListItems, 'ADE1'));
     });
 });
