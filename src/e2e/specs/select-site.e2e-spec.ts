d17d74d426485298ba4b304e8394ebc6358e4ce3
Brooke Smith <brooke.smith@ga.gov.au>
2017-06-27 11:51:58 +1000

    GEOD-514 - Refactor select-site e2e

--- src/e2e/specs/select-site.e2e-spec.ts
+++ src/e2e/specs/select-site.e2e-spec.ts
@@ -1,8 +1,11 @@
 import { browser } from 'protractor';
 import { SelectSitePage } from './select-site.pageobject';
+import { SiteLogPage } from './site-log.pageobject';
+import { TestUtils } from './test.utils';
 
 describe('SelectSite', () => {
     let selectSitePage: SelectSitePage = new SelectSitePage();
+    let siteLogPage: SiteLogPage = new SiteLogPage();
 
     beforeEach(async () => {
         return await browser.get(selectSitePage.url);
@@ -13,7 +16,7 @@ describe('SelectSite', () => {
         expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
         expect(selectSitePage.selectSiteListItems.count()).toEqual(1);
         expect(selectSitePage.selectSiteListItems.first().getText()).toBe('ADE1');
-        selectSitePage.debug(selectSitePage.selectSiteList);
+        TestUtils.debug(selectSitePage.selectSiteList);
     });
 
     it('entering search text but do not click on button should return list of sites - one site', () => {
@@ -21,14 +24,25 @@ describe('SelectSite', () => {
         expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
         expect(selectSitePage.selectSiteListItems.count()).toEqual(1);
         expect(selectSitePage.selectSiteListItems.first().getText()).toBe('ADE1');
-        selectSitePage.debug(selectSitePage.selectSiteList);
+        TestUtils.debug(selectSitePage.selectSiteList);
     });
 
     it('entering search text should return the specific sites - multiple sites', () => {
         selectSitePage.searchFor('ade');
         expect(selectSitePage.selectSiteList.isPresent()).toEqual(true);
-        expect(selectSitePage.selectSiteListItems.count()).toBeGreaterThan(1);
-        expect(selectSitePage.elementArrayContaining(selectSitePage.selectSiteListItems, 'ADE1').count()).toBe(1);
-        selectSitePage.debugArray(selectSitePage.elementArrayContaining(selectSitePage.selectSiteListItems, 'ADE1'));
+        expect(selectSitePage.selectSiteListItems.count()).not.toBe(0);
+        expect(TestUtils.elementArrayContaining(selectSitePage.selectSiteListItems, 'ADE1').count()).toBe(1);
+        TestUtils.debugArray(selectSitePage.selectSiteListItems);
+    });
+
+    it('selecting a specific site from the list should display the siteLog', () => {
+        selectSitePage.searchForClickOnSiteName('ADE1');
+        expect(siteLogPage.siteInformationHeader.isPresent()).toEqual(true, 'ADE1\'s siteLogPage.siteInformationHeader should exist');
+    });
+
+    it('selecting a general site string from the list should display the siteLog', () => {
+        selectSitePage.searchFor('ade');
+        selectSitePage.clickOnSite('ade1');
+        expect(siteLogPage.siteInformationHeader.isPresent()).toEqual(true, 'ADE1\'s siteLogPage.siteInformationHeader should exist');
     });
 });
