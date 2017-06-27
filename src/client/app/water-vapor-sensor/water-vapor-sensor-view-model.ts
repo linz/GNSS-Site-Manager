777107510843e6b4b628ed2eba92ef1c955a70df
lbodor <lazar.bodor@ga.gov.au>
2017-06-28 08:39:09 +1000

    Refactor water vapor sensor object map

--- src/client/app/water-vapor-sensor/water-vapor-sensor-view-model.ts
+++ src/client/app/water-vapor-sensor/water-vapor-sensor-view-model.ts
@@ -1,4 +1,5 @@
 import { AbstractViewModel } from '../shared/json-data-view-model/view-model/abstract-view-model';
+import { ObjectMap } from '../shared/json-data-view-model/data-view-translator';
 
 export class WaterVaporSensorViewModel extends AbstractViewModel {
     public calibrationDate: string = null;
@@ -11,28 +12,10 @@ export class WaterVaporSensorViewModel extends AbstractViewModel {
         super();
     }
 
+    public getObjectMap(): ObjectMap {
+        throw new Error('Not supported');
+    }
+
     createFieldMappings(): void {
-        this.addFieldMapping('/waterVaporSensor/validTime/abstractTimePrimitive/gml:TimePeriod/beginPosition/value/0',
-            'string',
-            '/startDate', 'date');
-
-        this.addFieldMapping('/waterVaporSensor/validTime/abstractTimePrimitive/gml:TimePeriod/endPosition/value/0',
-            'string',
-            '/endDate', 'date');
-
-        this.addFieldMapping('/waterVaporSensor/calibrationDate/value/0', 'string',
-            '/calibrationDate', 'date');
-
-        this.addFieldMapping('/waterVaporSensor/notes', 'string',
-            '/notes', 'string');
-
-        this.addFieldMapping('/waterVaporSensor/manufacturer', 'string',
-            '/manufacturer', 'string');
-
-        this.addFieldMapping('/waterVaporSensor/serialNumber', 'string',
-            '/serialNumber', 'string');
-
-        this.addFieldMapping('/waterVaporSensor/heightDiffToAntenna', 'string',
-            '/heightDiffToAntenna', 'number');
     };
 }
