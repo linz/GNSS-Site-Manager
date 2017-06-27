3f53d013a0cb0b0b8e80b0b6aa410ab1d25536f0
lbodor <lazar.bodor@ga.gov.au>
2017-06-28 08:52:22 +1000

    Refactor radio interference object map

--- src/client/app/radio-interference/radio-interference-view-model.ts
+++ src/client/app/radio-interference/radio-interference-view-model.ts
@@ -1,4 +1,5 @@
 import { AbstractViewModel } from '../shared/json-data-view-model/view-model/abstract-view-model';
+import { ObjectMap } from '../shared/json-data-view-model/data-view-translator';
 
 export class RadioInterferenceViewModel extends AbstractViewModel {
     public possibleProblemSource: string = null;
@@ -9,22 +10,10 @@ export class RadioInterferenceViewModel extends AbstractViewModel {
         super();
     }
 
+    public getObjectMap(): ObjectMap {
+        throw new Error('Not supported');
+    }
+
     createFieldMappings(): void {
-        this.addFieldMapping('/radioInterference/validTime/abstractTimePrimitive/gml:TimePeriod/beginPosition/value/0',
-            'string',
-            '/startDate', 'date');
-
-        this.addFieldMapping('/radioInterference/validTime/abstractTimePrimitive/gml:TimePeriod/endPosition/value/0',
-            'string',
-            '/endDate', 'date');
-
-        this.addFieldMapping('/radioInterference/possibleProblemSource', 'string',
-            '/possibleProblemSource', 'string');
-
-        this.addFieldMapping('/radioInterference/observedDegradation', 'string',
-            '/observedDegradation', 'string');
-
-        this.addFieldMapping('/radioInterference/notes', 'string',
-            '/notes', 'string');
     };
 }
