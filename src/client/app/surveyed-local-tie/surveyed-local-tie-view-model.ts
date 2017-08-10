bcc90b8ce6bd910d31f2d28d1965600b03d9893b
Frank Fu <frank.fu@ga.gov.au>
2017-08-10 14:58:17 +1000

    Add validation to dx/dy/dz and startDate in surveyed-local-tie group

--- src/client/app/surveyed-local-tie/surveyed-local-tie-view-model.ts
+++ src/client/app/surveyed-local-tie/surveyed-local-tie-view-model.ts
@@ -1,16 +1,20 @@
 import { AbstractViewModel } from '../shared/json-data-view-model/view-model/abstract-view-model';
 
+export class DifferentialComponent {
+    constructor(
+        public dx: number = null,
+        public dy: number = null,
+        public dz: number = null
+    ) {}
+}
+
 export class SurveyedLocalTieViewModel extends AbstractViewModel {
 
     public tiedMarkerName: string = null;
     public tiedMarkerUsage: string = null;
     public tiedMarkerCDPNumber: string = null;
     public tiedMarkerDOMESNumber: string = null;
-
-    public dx: number = null;
-    public dy: number = null;
-    public dz: number = null;
-
+    public differentialComponent: DifferentialComponent = new DifferentialComponent();
     public localSiteTiesAccuracy: string = null;
     public surveyMethod: string = null;
     public notes: string = null;
