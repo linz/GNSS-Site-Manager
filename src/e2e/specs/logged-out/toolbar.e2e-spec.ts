82c5f5e5c83c7d4c404149493718f8017709ad15
Frank Fu <frank.fu@ga.gov.au>
2017-08-14 11:13:17 +1000

    Merge e2e tests on toolbar

--- src/e2e/specs/logged-out/toolbar.e2e-spec.ts
+++ src/e2e/specs/logged-out/toolbar.e2e-spec.ts
@@ -46,4 +46,26 @@ describe('Toolbar', () => {
         expect(siteLogPage.newSiteLink.getAttribute('class')).toContain('disabled', 'New Site Link should be disabled');
         expect(siteLogPage.aboutLink.isPresent()).toEqual(true);
     });
+
+    it('should have the correct menus when logged in and viewing a site', () => {
+        loginActions.login('user.a', 'gumby123A');
+
+        let siteLogPage: SiteLogPage = selectSitePage.openSite('ADE1');
+
+        expect(siteLogPage.siteIdMenu.isPresent()).toEqual(true);
+        expect(siteLogPage.saveSiteLink.isPresent()).toEqual(true);
+        expect(siteLogPage.revertSiteLink.isPresent()).toEqual(true);
+        expect(siteLogPage.closeSiteLink.isPresent()).toEqual(true);
+
+        expect(siteLogPage.loginMenu.isPresent()).toEqual(true);
+        expect(siteLogPage.profileLink.isPresent()).toEqual(true);
+        expect(siteLogPage.changePasswordLink.isPresent()).toEqual(true);
+        expect(siteLogPage.logoutLink.isPresent()).toEqual(true);
+
+        expect(siteLogPage.navigationMenu.isPresent()).toEqual(true);
+        expect(siteLogPage.selectSiteLink.isPresent()).toEqual(true);
+        expect(siteLogPage.newSiteLink.isPresent()).toEqual(true);
+        expect(siteLogPage.newSiteLink.getAttribute('class')).not.toContain('disabled', 'New Site Link should be enabled');
+        expect(siteLogPage.aboutLink.isPresent()).toEqual(true);
+    });
 });
