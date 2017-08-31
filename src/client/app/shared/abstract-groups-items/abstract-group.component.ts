linz/GNSS-Site-Manager

commit 9cd437d4519ec85fbcf59ad81abf34210e540e27

Don't remove deleted log items from the site log
Change tracking fields `dateDeleted` and `reasonDeleted` are used to
mark log items as deleted.

src/client/app/shared/abstract-groups-items/abstract-group.component.ts
@@ -1,4 +1,4 @@
-import { Input, OnInit } from '@angular/core';
+import { Input, OnChanges, SimpleChange } from '@angular/core';
 import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import { AbstractBaseComponent } from './abstract-base.component';
 import { GeodesyEvent, EventNames } from '../events-messages/Event';
@@ -10,7 +10,7 @@ import * as _ from 'lodash';
 
 export const newItemShouldBeBlank: boolean = true;
 
-export abstract class AbstractGroupComponent<T extends AbstractViewModel> extends AbstractBaseComponent implements OnInit {
+export abstract class AbstractGroupComponent<T extends AbstractViewModel> extends AbstractBaseComponent implements OnChanges {
     isGroupOpen: boolean = false;
 
     // flag to indicate that the current or latest item in a group has an end date set
@@ -65,8 +65,10 @@ export abstract class AbstractGroupComponent<T extends AbstractViewModel> extend
         super(userAuthService);
     }
 
-    ngOnInit() {
-        this.setupForm();
+    ngOnChanges(changes: { [property: string]: SimpleChange }) {
+        if (changes['siteLogModel']) {
+            this.setupForm();
+        }
     }
 
     /**
@@ -105,7 +107,10 @@ export abstract class AbstractGroupComponent<T extends AbstractViewModel> extend
 
     setItems(items: T[]) {
         if (items) {
-            this.items = items;
+            items.forEach((element: T, i: number) => {
+                element.id = i;
+            });
+            this.items = _.filter(items, item => !item.dateDeleted);
             this.items.sort(AbstractGroupComponent.compare);
             this.items.forEach(() => {
                 this.groupArrayForm.insert(0, new FormGroup({}));

src/client/app/frequency-standard/frequency-standard-item.component.ts
@@ -41,6 +41,7 @@ export class FrequencyStandardItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {standardType: new FormControl('', [Validators.maxLength(200)])},
             {inputFrequency: new FormControl('', [Validators.maxLength(25)])},
             {startDate: new FormControl('')},   // Validators wont work in the DateTime custom component

src/client/app/gnss-antenna/gnss-antenna-item.component.ts
@@ -41,6 +41,7 @@ export class GnssAntennaItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {antennaType: new FormControl('', [Validators.maxLength(100)])},
             {serialNumber: new FormControl('', [Validators.maxLength(50)])},
             {startDate: new FormControl('')},   // Validators wont work in the DateTime custom component

src/client/app/gnss-receiver/gnss-receiver-item.component.ts
@@ -33,6 +33,7 @@ export class GnssReceiverItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {receiverType: new FormControl(' ', [Validators.maxLength(25)])},
             {manufacturerSerialNumber: new FormControl('', [Validators.maxLength(25)])},
             {startDate: new FormControl('')},   // Validators wont work in the DateTime custom component

src/client/app/humidity-sensor/humidity-sensor-item.component.ts
@@ -44,6 +44,7 @@ export class HumiditySensorItemComponent extends AbstractItemComponent {
         // turn off all Validators until work out solution to 'was false now true' problem
         // TODO Fix Validators
         return new ItemControls([
+            {id: new FormControl(null)},
             {manufacturer: new FormControl('', [Validators.required, Validators.maxLength(50)])},
             {serialNumber: new FormControl('', [Validators.required, Validators.maxLength(50)])},
             {dataSamplingInterval: new FormControl('', [Validators.maxLength(25)])},

src/client/app/local-episodic-effect/local-episodic-effect-item.component.ts
@@ -41,6 +41,7 @@ export class LocalEpisodicEffectItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {event: new FormControl('', [Validators.required, Validators.maxLength(100)])},
             {startDate: new FormControl('')},   // Validators wont work in the DateTime custom component
             {endDate: new FormControl('')},

src/client/app/multipath-source/multipath-source-item.component.ts
@@ -41,6 +41,7 @@ export class MultipathSourceItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {possibleProblemSource: new FormControl('', [Validators.maxLength(100)])},
             {startDate: new FormControl('')},   // Validators wont work in the DateTime custom component
             {endDate: new FormControl('')},

src/client/app/pressure-sensor/pressure-sensor-item.component.ts
@@ -41,6 +41,7 @@ export class PressureSensorItemComponent extends AbstractItemComponent {
      */
     getFormControls(): ItemControls {
         return new ItemControls([
+            {id: new FormControl(null)},
             {manufacturer: new FormControl('', [Validators.required, Validators.maxLength(100)])},
             {serialNumber: new FormControl('', [Validators.required, Validators.maxLength(100)])},
             {dataSamplingInterval: new FormControl('', [Validators.maxLength(25)])},

commit ac8ee05e4252e5397674e34f96d8e15166b6396b

Fix inital-dirty-flag error caused by previous commit 9cd437d4519ec85fbcf59ad81abf34210e540e27

src/client/app/shared/abstract-groups-items/abstract-group.component.ts
@@ -1,4 +1,4 @@
-import { Input, AfterViewInit, OnChanges, SimpleChange, ViewChildren, QueryList } from '@angular/core';
+import { Input, OnInit, AfterViewInit, OnChanges, SimpleChange, ViewChildren, QueryList } from '@angular/core';
 import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import { AbstractBaseComponent } from './abstract-base.component';
 import { AbstractItemComponent } from './abstract-item.component';
@@ -13,7 +13,7 @@ export const newItemShouldBeBlank: boolean = true;
 
 export abstract class AbstractGroupComponent<T extends AbstractViewModel>
     extends AbstractBaseComponent
-    implements AfterViewInit, OnChanges {
+    implements OnInit, AfterViewInit, OnChanges {
 
     isGroupOpen: boolean = false;
 
@@ -70,6 +70,10 @@ export abstract class AbstractGroupComponent<T extends AbstractViewModel>
         super(siteLogService);
     }
 
+    ngOnInit() {
+        this.setupForm();
+    }
+
     ngAfterViewInit() {
         if (this.allowOneCurrentItem()) {
             setTimeout(() => {
@@ -90,7 +94,7 @@ export abstract class AbstractGroupComponent<T extends AbstractViewModel>
     }
 
     ngOnChanges(changes: { [property: string]: SimpleChange }) {
-        if (changes['siteLogModel']) {
+        if (changes['siteLogModel'] && !changes['siteLogModel'].isFirstChange()) {
             this.setupForm();
         }
     }